0ea4987be7667816ffee956719f8ec27f5dfca26
Aleksander Kaminski <akaminski2010@gmail.com>
2015-07-03 18:14:55 +0100

    Added more listeners to 'tex' object.

--- web/newdart.dart
+++ web/newdart.dart
@@ -6,9 +6,11 @@ class testApplication {
   var renderer = new CanvasRenderer(width: 800, height: 600);
   var stage = new Stage(new Colour.fromHtml('#6f9'));
   var tex = new Sprite.fromImage('res/texture.png');
+  int i = 0;
 
   testApplication(){
-    document.body.append(this.renderer.view);
+
+    document.getElementById("viewContainer").append(this.renderer.view);
 
     this.tex.anchor = new Point(0.5, 0.5);
     this.tex.position = new Point(300,300);
@@ -16,21 +18,32 @@ class testApplication {
 
     querySelector("#text").text = "This Should change";
 
-    this.tex.onMouseDown.listen((e) => querySelector("#text").text = "This has changed!");
-    this.tex.onTap.listen((e) => print("Clicked button!"));
+    tex.onMouseDown.listen((e)	{ tex.alpha = 0.9; tex.dragging = true; });
+    tex.onTouchStart.listen((e)	{ tex.alpha = 0.9; tex.dragging = true; });
+    tex.onMouseUp.listen((e) 		{ tex.alpha = 1.0; tex.dragging = false; });
+    tex.onTouchEnd.listen((e) 	{ tex.alpha = 1.0; tex.dragging = false; });
+    tex.onMouseMove.listen((e)	{ if (tex.dragging) tex.position = new Point(e.x, e.y); });
+    tex.onTouchMove.listen((e)	{ if (tex.dragging) tex.position = new Point(e.x, e.y); });
+
 
     this.stage.children.add(this.tex);
 
-    window.requestAnimationFrame(this._animate);
 
+    window.requestAnimationFrame(this._animate);
 
   }
 
+  void changeText(){
+    i++;
+    querySelector("#text").text = "This has changed! Iteration ("+i.toString()+")";
+
+  }
 
   void _animate(var num)
   {
+
+
     window.requestAnimationFrame(this._animate);
-    this.tex.rotation += -0.1;
     this.renderer.render(this.stage);
 
   }
