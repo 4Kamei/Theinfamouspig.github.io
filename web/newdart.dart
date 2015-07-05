2347349c7d78bd5c0977c7c8879185d40d9fa948
Aleksander Kaminski <akaminski2010@gmail.com>
2015-07-05 19:19:56 +0100

    ixdex.html

--- web/newdart.dart
+++ web/newdart.dart
@@ -4,7 +4,7 @@ import 'package:pixi/pixi.dart';
 class testApplication {
 
   var renderer = new CanvasRenderer(width: 800, height: 800);
-  var stage = new Stage(new Colour.fromHtml('#f69'));
+  var stage = new Stage(new Colour.fromHtml('#553333'));
   var tex = new List(9);
   var holder = new DisplayObjectContainer();
 
@@ -19,15 +19,13 @@ class testApplication {
     document.getElementById("viewContainer").append(this.renderer.view);
     querySelector("#text").text = "This Should change";
 
-
-
     holder.position = new Point(400,400);
 
     for(int j = 0; j < 9; j++){
       tex[j].anchor = new Point(0.5,0.5);
       holder.children.add(tex[j]);
-
     }
+
     tex[0].position = new Point(0,0);
     tex[1].position = new Point(-200,-200);
     tex[2].position = new Point(-282,0);
@@ -38,13 +36,12 @@ class testApplication {
     tex[7].position = new Point(282,0);
     tex[8].position = new Point(200,200);
 
-
     for(int j = 0; j < 9; j++) {
       tex[j].anchor = new Point(0.5, 0.5);
       holder.children.add(tex[j]);
     }
-    stage.children.add(holder);
 
+    stage.children.add(holder);
     window.requestAnimationFrame(this._animate);
 
   }
@@ -52,9 +49,11 @@ class testApplication {
   void _animate(var num)
   {
     window.requestAnimationFrame(this._animate);
-    for(int j = 0; j < 9; j++)
+    for(int j = 0; j < 9; j++){
       tex[j].rotation += 0.03;
-    tex[0].rotation += 0.03;
+    }
+      tex[0].rotation += 0.03;
+
     holder.rotation -= 0.03;
     this.renderer.render(this.stage);
   }
